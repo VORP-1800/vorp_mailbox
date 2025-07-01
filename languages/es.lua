d37295b62815e8f115d11ee7d2fcca593cf1e341
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2025-07-01 16:17:13 -0500

    lang

---
+++ languages/es.lua
@@ -0,0 +1,20 @@
+Locales['es'] = {
+    ["TipOnMessageReceived"] = "¡Oye tú! Acabas de recibir un mensaje de $1",
+    ["TipOnBroadcastReceived"] = "Nuevo telegrama:\n$1\nMensaje pagado por $2",
+    ["TipOnMessageSent"] = "Telegrama enviado",
+    ["TipOnBroadcastSent"] = "Telegrama enviado",
+    ["TipOnTooRecentMessageSent"] = "Tienes que esperar al menos $1 seg antes de enviar un nuevo mensaje", -- no tocar $n, se reemplaza con valores en tiempo de ejecución
+    ["TipOnInsufficientMoneyForMessage"] = "Dinero insuficiente. Necesitas $1$", -- no tocar $n, se reemplaza con valores en tiempo de ejecución
+    ["TipOnInsufficientMoneyForBroadcast"] = "Dinero insuficiente. Necesitas $1$", -- no tocar $n, se reemplaza con valores en tiempo de ejecución
+    ["TextNearMailboxLocation"] = "Presiona $1 para ver tu correo o $2 para enviar un telegrama a todos", -- no tocar $, se reemplaza con la tecla en tiempo de ejecución
+    ["UICloseButton"] = "Cerrar",
+    ["UIWriteButton"] = "Escribir",
+    ["UIDeleteButton"] = "Eliminar",
+    ["UIAnswerButton"] = "Responder",
+    ["UIAbortButton"] = "Cancelar",
+    ["UISendButton"] = "Enviar",
+    ["UINamePrefix"] = "De",
+    ["UITitle"] = "Buzón",
+    ["UINoMessageReceived"] = "No se ha recibido ningún mensaje"
+}
+
